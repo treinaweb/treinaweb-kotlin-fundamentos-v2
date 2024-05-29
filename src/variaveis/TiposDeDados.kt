915f8cb8941d1b22692f5f8420e1228ced42ba73
Cleyson Lima <cleysonph@gmail.com>
2024-05-29 14:30:39 -0300

    02:07 - Os tipos de dados do Kotlin

---
+++ src/variaveis/TiposDeDados.kt
@@ -0,0 +1,46 @@
+package variaveis
+
+fun main() {
+    // Tipo númericos
+    /*
+     * Byte - 8
+     * Short - 16
+     * Int - 32
+     * Long - 64
+     * Float - 32
+     * Double - 64
+     */
+
+    val byte: Byte = 8
+    val short: Short = 360
+    val int: Int = 1000
+    val long: Long = 10000L
+    val float: Float = 150.10F
+    val double: Double = 300.10
+
+    println(byte)
+    println(short)
+    println(int)
+    println(long)
+    println(float)
+    println(double)
+
+    // Tipo Booleano
+    // true false
+
+    val boolean: Boolean = false
+
+    println(boolean)
+
+    // Tipos textuais
+    /*
+     * Char
+     * String
+     */
+
+    val char: Char = 'c'
+    val string: String = "Cleyson"
+
+    println(char)
+    println(string)
+}
