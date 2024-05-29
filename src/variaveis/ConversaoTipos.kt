c90eed82eaa0095f6b71406a4f4bbdf6f517a6ba
Cleyson Lima <cleysonph@gmail.com>
2024-05-29 14:52:56 -0300

    02:08 - Conversão de tipos

---
+++ src/variaveis/ConversaoTipos.kt
@@ -0,0 +1,32 @@
+package variaveis
+
+fun main() {
+    val i1 = 10
+    val l1 = i1.toLong()
+
+    println(i1)
+    println(l1)
+
+    val d1 = i1.toDouble()
+
+    println(d1)
+
+    val d2 = 25.71
+    val i2 = d2.toInt()
+
+    println(d2)
+    println(i2)
+
+    val i3 = 10000
+    val b1 = i3.toByte()
+
+    println(i3)
+    println(b1)
+
+    val s1 = i1.toString()
+    val s2 = "101"
+    val i4 = s2.toInt()
+
+    println(s1)
+    println(i4)
+}
