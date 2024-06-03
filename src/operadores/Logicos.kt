22cceb31d0f073d899ec3084218db3ebdc5893d7
Cleyson Lima <cleysonph@gmail.com>
2024-06-03 13:38:05 -0300

    03:12 - Operadores lógicos

---
+++ src/operadores/Logicos.kt
@@ -0,0 +1,23 @@
+package operadores
+
+fun main() {
+    // Conjunção (E)
+    println(true && true)
+    println(true && false)
+    println(false && true)
+    println(false && false)
+
+    println("----------------------------------")
+
+    // Disjunção (OU)
+    println(true || true)
+    println(false || true)
+    println(true || false)
+    println(false || false)
+
+    println("----------------------------------")
+
+    // Negação (NÃO)
+    println(!true)
+    println(!false)
+}
