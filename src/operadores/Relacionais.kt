cd51eaa1bd417d2ad8ffccd75b239a996ddc4f0b
Cleyson Lima <cleysonph@gmail.com>
2024-06-03 13:30:29 -0300

    03:11 - Operadores relacionais

---
+++ src/operadores/Relacionais.kt
@@ -0,0 +1,23 @@
+package operadores
+
+fun main() {
+    val a = 10
+    val b = 5
+    val c = 5
+
+    // Maior que
+    println(a > b)
+
+    // Menor que
+    println(a < b)
+
+    // Maior ou igual
+    println(c >= a)
+
+    // Menor ou igaul
+    println(c <= a)
+
+    // Igual
+    println(b == c)
+    println(b === c)
+}
