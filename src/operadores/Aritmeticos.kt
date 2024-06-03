ecb2088a850fae9b2ac33720934f68547951930a
Cleyson Lima <cleysonph@gmail.com>
2024-06-03 13:21:49 -0300

    03:10 - Operadores aritméticos

---
+++ src/operadores/Aritmeticos.kt
@@ -0,0 +1,25 @@
+package operadores
+
+fun main() {
+    val a = 5
+    val b = 2
+
+    // Soma
+    println(a + b)
+
+    // Subtração
+    println(a - b)
+
+    // Multiplicação
+    println(a * b)
+
+    // Divisão
+    println(a / b.toDouble())
+
+    // Modulo
+    println(a % b)
+
+    // Concatenação
+    val c = "Cleyson " + "Lima"
+    println(c)
+}
